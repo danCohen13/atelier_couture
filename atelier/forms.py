9b01a9ef50b362f38c56a12ee94a5b94f46e07fd
Dan <dan.cohendan13110@gmail.com>
2026-07-09 22:56:34 +0300

    Design: Passage a l identite visuelle Patron d Atelier

--- atelier/forms.py
+++ atelier/forms.py
@@ -4,20 +4,18 @@ from .models import Client, Robe
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = '__all__'  # Inclut tous les champs de la table Client
+        fields = '__all__'
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Boucle automatique pour appliquer le style Tailwind sur chaque champ
         for field in self.fields.values():
-            field.widget.attrs.update({'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm border p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'})
+            field.widget.attrs.update({'class': 'field-input'})
 
 class RobeForm(forms.ModelForm):
     class Meta:
         model = Robe
         fields = '__all__'
         widgets = {
-            # On force le navigateur à afficher un vrai calendrier pour les dates
             'date_commencement': forms.DateInput(attrs={'type': 'date'}),
             'date_livraison': forms.DateInput(attrs={'type': 'date'}),
         }
@@ -25,4 +23,4 @@ class RobeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs.update({'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm border p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'})
+            field.widget.attrs.update({'class': 'field-input'})
