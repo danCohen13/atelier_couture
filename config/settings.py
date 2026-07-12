74c1f263bb2f12f3a52e695f4b40cf5760dd28f2
Dan <dan.cohendan13110@gmail.com>
2026-07-12 13:01:07 +0300

    Fix: ajout de STATICFILES_DIRS pour lier le CSS global en production

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Dossier où Django va rassembler tous les assets pour la production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
